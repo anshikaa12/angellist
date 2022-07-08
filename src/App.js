1890fa96220a7692411e0cdeeab249a95a40db78
Anshika Agarwal <agarwal.anshika100@gmail.com>
2022-07-08 16:31:56 +0530

    fix issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages";
 function App() {
-  return <div className="App">
-    <Routes>
-    <Route path="/" element={<Home/>} />
-    </Routes>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
