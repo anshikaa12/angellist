794069e55b32e6400e624146f76c479cb4aa48a5
Anshika Agarwal <agarwal.anshika100@gmail.com>
2022-07-08 17:30:10 +0530

    database
    >>made the databes
    >>made the filters

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,9 +1,35 @@
 import React from "react";
-
+import { houses } from "../database/houses";
 function Card() {
   return (
     <div className="card-sec column-3-grid">
-      <div className="basic-card">
+      {houses.map((house) => {
+        return (
+          <div className="basic-card">
+            <img src={house.img} className="card-img" alt="text" />
+            <div className="card-body">
+              <span className="card-price h4-text wt-bold">{house.price}$</span>
+              <h4 className="card-title">{house.name}</h4>
+              <p className="card-content">{house.address}.</p>
+              <div className="card-footer">
+                <div className="card-foot flex-row-center">
+                  <i class="fas fa-bed"></i>
+                  <p className="foot-text">{house.beds} bedroom</p>
+                </div>
+                <div className="card-foot flex-row-center">
+                  <i class="fas fa-sink"></i>
+                  <p className="foot-text">2 bathroom</p>
+                </div>
+                <div className="card-foot flex-row-center">
+                  <i class="fas fa-cube"></i>
+                  <p className="foot-text">5X7 m</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      {/* <div className="basic-card">
         <img
           src="https://res.cloudinary.com/de5it79dw/image/upload/v1657274189/download_5_xk2vds.jpg"
           className="card-img"
@@ -79,8 +105,8 @@ function Card() {
               <p className="foot-text">5X7 m</p>
             </div>
           </div>
-        </div>
-      </div>
+        </div> */}
+      {/* </div> */}
     </div>
   );
 }
